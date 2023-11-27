e87f44efe507bc2f43d5176efe219be3165d5da8
Sadampayoda <sadampayodaa@gmail.com>
2023-11-27 10:50:19 +0700

    revisi

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+subprocess.run(["pip", "install", "matplotlib"])
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import subprocess
+
+# Instal matplotlib
 
 st.title("Aplikasi Prediksi")
-
 
 
 
